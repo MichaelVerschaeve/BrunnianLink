8d3b07c7f12ea7902cb93d7e5b7a30c38e2593bd
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2023-05-22 20:09:00 +0200

    shield rules added, plus shield.io

--- ShieldRule.cs
+++ ShieldRule.cs
@@ -54,8 +54,18 @@ namespace BrunnianLink
             new List<(double x, double y, double rotation, int state)>
             {
                 (0,1.0+Math.Sqrt(3.0),0,7), //center triangle
-
-
+                (-0.5,1.0+Math.Sqrt(3.0)*0.5,-30,5), //left top square
+                (0.5,1.0+Math.Sqrt(3.0)*0.5,30,2), //right top square
+                (0.5,1.0+Math.Sqrt(3.0)*0.5,90,5), //bottom square
+                (-0.5,Math.Sqrt(3.0)*0.5,60,0), //bottom outer triangle
+                (-Math.Sqrt(3.0),1.5+Math.Sqrt(3.0),-60,0), //left outer triangle
+                (Math.Sqrt(3.0),1.5+Math.Sqrt(3.0),60,7), //right outer triangle
+                (-0.5*Math.Sqrt(3.0),2+Math.Sqrt(3.0),90,7), //duo on top, left triangle
+                (0.5*Math.Sqrt(3.0),2+Math.Sqrt(3.0),-90,1), //duo on top, right,triangle
+                (-0.5*(1+Math.Sqrt(3.0)),1.5+0.5*Math.Sqrt(3.0),30,0), //duo on left, top triangle
+                (-0.5,0.5*Math.Sqrt(3.0),-150,6), //duo on left, bottom triangle
+                (0.5*(1+Math.Sqrt(3.0)),1.5+0.5*Math.Sqrt(3.0),-30,7), //duo on right, top triangle
+                (0.5,0.5*Math.Sqrt(3.0),150,1), //duo on right, bottom triangle
             }
         };
 
