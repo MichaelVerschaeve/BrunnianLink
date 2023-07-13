a8651d49771a890cb3c1b8a5abf72a034c0ce42a
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2023-07-13 09:42:54 +0200

    fibbo flake

--- OctaFlake.cs
+++ OctaFlake.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,28 +14,92 @@ namespace BrunnianLink
 
         static private readonly double rad = 3.6;
 
-        public static void Generate(StringBuilder sb, int level)
-        {
-            double x = 0;
-            double y = 0;
-            int rot = 0;
-            for (int i = 0; i < 8; i++)
-            {
-                Recurse(sb, level, true,ref x, ref y, ref rot);
-            }
-        }
 
         static private readonly int yellowId = ColorMap.Get("Yellow").id;
         static private readonly int redId = ColorMap.Get("Red").id;
 
-        static private void Recurse(StringBuilder sb, int level, bool left, ref double x, ref double y, ref int rotation)
+        /* public static void Generate(StringBuilder sb, int level)
+         {
+             double x = 0;
+             double y = 0;
+             int rot = 0;
+             for (int i = 0; i < 8; i++)
+             {
+                 Recurse(sb, level, true,ref x, ref y, ref rot);
+             }
+         }
+
+
+         static private void Recurse(StringBuilder sb, int level, bool left, ref double x, ref double y, ref int rotation)
+         {
+             if (level == 0)
+             {
+                 sb.AppendLine(axle.Rotate(rotation * 45).Print(x, y, 0, redId));
+                 if (left) 
+                     sb.AppendLine(elbow.Rotate(rotation * 45).Print(x, y, 0, yellowId));
+
+                 double oldAngle = Math.PI * rotation * 0.25;
+                 int sign = left ? 1 : -1;
+                 x += rad * Math.Cos(oldAngle + sign * 0.5 * Math.PI);
+                 y += rad * Math.Sin(oldAngle + sign * 0.5 * Math.PI);
+                 if (left)
+                 {
+                     rotation++;
+                     if (rotation > 4)
+                         rotation -= 8;
+                 }
+                 else
+                 {
+                     rotation--;
+                     if (rotation <= -4)
+                         rotation += 8;
+                 }
+                 double newAngle = Math.PI * rotation * 0.25;
+                 x += rad * Math.Cos(newAngle - sign * 0.5 * Math.PI);
+                 y += rad * Math.Sin(newAngle - sign * 0.5 * Math.PI);
+
+                 if (!left)
+                     sb.AppendLine(elbow.Rotate(rotation * 45+180).Print(x, y, 0, yellowId));
+                 return;
+             }
+             for (int i = 0; i < 3; i++)
+                 Recurse(sb, level - 1, left, ref x, ref y, ref rotation);
+             for (int i = 0; i < 5; i++)
+                 Recurse(sb, level - 1, !left, ref x, ref y, ref rotation);
+             for (int i = 0; i < 3; i++)
+                 Recurse(sb, level - 1, left, ref x, ref y, ref rotation);
+
+         }*/
+
+        public static void Generate(StringBuilder sb, int level)
         {
-            if (level == 0)
+            MetaData.StartSubModel(sb, $"FibbonacciWordFractal_{level}");
+            List<bool> moves = new() { false, false }; 
+            for (int i = 1;i < level;i++)
+            {
+                List<bool> newmoves = new();
+                bool invert = true;
+                foreach (bool move in moves)
+                {
+                    if (invert)
+                        newmoves.AddRange(Enumerable.Repeat(!move, 3));
+                    else
+                        newmoves.AddRange(Enumerable.Repeat(move, 5));
+                    invert = !invert;
+                }
+                moves = newmoves.Skip(1).Append(newmoves.First()).ToList();
+            }
+            moves = moves.Concat(moves).Concat(moves).Concat(moves).ToList();
+
+            double x = 0;
+            double y = 0;
+            int rotation = 0;
+
+            foreach (bool left in moves)
             {
                 sb.AppendLine(axle.Rotate(rotation * 45).Print(x, y, 0, redId));
-                if (left) 
+                if (left)
                     sb.AppendLine(elbow.Rotate(rotation * 45).Print(x, y, 0, yellowId));
-
                 double oldAngle = Math.PI * rotation * 0.25;
                 int sign = left ? 1 : -1;
                 x += rad * Math.Cos(oldAngle + sign * 0.5 * Math.PI);
@@ -56,15 +121,8 @@ namespace BrunnianLink
                 y += rad * Math.Sin(newAngle - sign * 0.5 * Math.PI);
 
                 if (!left)
-                    sb.AppendLine(elbow.Rotate(rotation * 45+180).Print(x, y, 0, yellowId));
-                return;
+                    sb.AppendLine(elbow.Rotate(rotation * 45 + 180).Print(x, y, 0, yellowId));
             }
-            for (int i = 0; i < 3; i++)
-                Recurse(sb, level - 1, left, ref x, ref y, ref rotation);
-            for (int i = 0; i < 5; i++)
-                Recurse(sb, level - 1, !left, ref x, ref y, ref rotation);
-            for (int i = 0; i < 3; i++)
-                Recurse(sb, level - 1, left, ref x, ref y, ref rotation);
 
         }
 
