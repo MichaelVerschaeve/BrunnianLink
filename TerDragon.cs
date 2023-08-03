e5a0760afd6509af998f168c4604147d9bab1174
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2023-08-03 09:36:15 +0200

    terdragon fixes

--- TerDragon.cs
+++ TerDragon.cs
@@ -18,29 +18,44 @@ namespace BrunnianLink
             for (int i = 0; i < level; i++)
                 moves = moves.Append(false).Concat(moves).Append(true).Concat(moves).ToList();
             int idGreen = ColorMap.Get("Green").id;
+
+
             Shape leftTurn = new() { PartID = "left_turn", SubModel = true };
             Shape rightTurn = new() { PartID = "right_turn", SubModel = true };
+
             double s60 = Math.Sqrt(3.0) * 0.5;
-            List<string> someColorNames = new() { "Red","Blue","Green","Orange","Purple","Black" };
+            List<string> someColorNames = new() { "Red", "Blue", "Green", "Orange", "Purple", "Black" };
             var colorIds = someColorNames.Select(x => ColorMap.Get(x).id).ToList();
             for (int rotation = 0; rotation < 6; rotation++)
-                sb.AppendLine(new Shape() { PartID = $"rot{rotation}", SubModel=true }.Print(0, 0, 0, colorIds[rotation]));
+                sb.AppendLine(new Shape() { PartID = $"rot{rotation}", SubModel = true }.Print(0, 0, 0, 16));
             double d = Math.Sqrt(3.0) + 2;
+            double[] c = new[] { d, 0.5 * d, -0.5 * d, -d, -0.5 * d, 0.5 * d };
+            double[] s = new[] { 0, s60 * d, s60 * d, 0, -s60 * d, -s60 * d, 0 };
 
-            for (int rotation = 0; rotation < 6; rotation++)
+            List<Shape> leftTurnsTop = someColorNames.Select(x => new Shape() { PartID = $"left_turn_top_{x}", SubModel = true }).ToList();
+            List<Shape> rightTurnsTop = someColorNames.Select(x => new Shape() { PartID = $"right_turn_top_{x}", SubModel = true }).ToList();
+
+            List<Shape> leftTurnsBottom = someColorNames.Select(x => new Shape() { PartID = $"left_turn_bottom_{x}", SubModel = true }).ToList();
+            List<Shape> rightTurnsBottom = someColorNames.Select(x => new Shape() { PartID = $"right_turn_bottom_{x}", SubModel = true }).ToList(); ;
+
+            for (int initialrotation = 0; initialrotation < 6; initialrotation++)
             {
+                int rotation = initialrotation;
                 MetaData.StartSubModel(sb, $"rot{rotation}");
-                double x = d;
-                double y = 0;
 
-                double[] c = new[] { d, 0.5 * d, -0.5 * d, -d, -0.5 * d, 0.5 * d };
-                double[] s = new[] { 0, s60 * d, s60 * d, 0, -s60 * d, -s60 * d, 0 };
+                Shape leftTurnTop = leftTurnsTop[initialrotation];
+                Shape rightTurnTop = rightTurnsTop[initialrotation];
+                Shape leftTurnBottom = leftTurnsBottom[initialrotation];
+                Shape rightTurnBottom = rightTurnsBottom[initialrotation];
+
+                double x = c[rotation];
+                double y = s[rotation];
 
                 bool top = false;
 
                 foreach (bool left in moves)
                 {
-                    sb.AppendLine((left ? leftTurn : rightTurn).Rotate(rotation * 60).Print(x, y, top ? 0.0 : -1.0, 16));
+                    sb.AppendLine((top ? (left ? leftTurnTop : rightTurnTop) : (left ? leftTurnBottom : rightTurnBottom)).Rotate(rotation * 60).Print(x, y, 0.0, 16));
                     if (left)
                     {
                         rotation += 2;
@@ -58,11 +73,24 @@ namespace BrunnianLink
                     top = !top;
                 }
             }
-            MetaData.StartSubModel(sb, "left_turn");
-            sb.AppendLine(rightTurn.Rotate(-60).Print(0, 0,0,16));
-            MetaData.StartSubModel(sb, "right_turn");
-            sb.AppendLine(new Shape() { PartID = "2429" }.Print(-s60,-0.5,0,16));
-            sb.AppendLine(new Shape() { PartID = "2430" }.Rotate(-120).Print(-s60, -0.5, 0, 16));
+            int colorId_base1 = ColorMap.Get("Light_Bluish_Grey").id;
+            int colorId_base2 = ColorMap.Get("White").id;
+            foreach (string color in someColorNames)
+            {
+                int colorId = ColorMap.Get(color).id;
+                foreach (string top in new string[] { "top", "bottom" })
+                {
+                    MetaData.StartSubModel(sb, $"left_turn_{top}_{color}");
+                    string rightShapeId = $"right_turn_{top}_{color}";
+                    sb.AppendLine(new Shape() { PartID = rightShapeId, SubModel = true }.Rotate(-60).Print(0, 0, 0, colorId_base1));
+                    MetaData.StartSubModel(sb, rightShapeId);
+                    sb.AppendLine(new Shape() { PartID = "2429" }.Print(-s60, -0.5, top == "top" ? 0 : -1, colorId_base1));
+                    sb.AppendLine(new Shape() { PartID = "2430" }.Rotate(-120).Print(-s60, -0.5, top == "top" ? 0 : -1, colorId_base1));
+                    sb.AppendLine(new Shape() { PartID = "25269" }.Rotate(90).Print(-s60 - 0.5, 0.0, 1, colorId));
+                    sb.AppendLine(new Shape() { PartID = "25269" }.Rotate(60).Print(-s60*0.5 - 0.25, -0.5*s60- 0.75, 1, colorId));
+                }
+                (colorId_base1, colorId_base2) = (colorId_base2, colorId_base1);
+            }
         }
     }
 }
