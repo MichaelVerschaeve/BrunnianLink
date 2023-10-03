cd91289da5ee5966274c439fb6295fa1952efb10
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2023-10-03 08:52:01 +0200

    progress in code to approximate circles with slopes

--- SphereGenerator.cs
+++ SphereGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace BrunnianLink
 
         private const double epsilon = 1.0-7;
 
-        struct Slope
+        class Slope
         {
             public int leftmargin;
             public int bottommargin;
@@ -26,9 +27,12 @@ namespace BrunnianLink
             public int dy;
         }
 
+        static Slope horzSlope = new Slope { leftmargin = 0, bottommargin = 0, dx = 1, dy = 0 };
+        static Slope vertSlope = new Slope { leftmargin = 0, bottommargin = 0, dx = 0, dy = 1 };
+
         static List<Slope> m_slopes = new List<Slope>()
         {
-            new Slope { leftmargin = 0, bottommargin = 0, dx = 1, dy = 0 },
+            horzSlope,
             new Slope { leftmargin = 1, bottommargin = 1, dx = 1, dy = 1 },
             new Slope { leftmargin = 1, bottommargin = 1, dx = 2, dy = 2 },
             new Slope { leftmargin = 1, bottommargin = 1, dx = 3, dy = 3 },
@@ -40,23 +44,73 @@ namespace BrunnianLink
             new Slope { leftmargin = 1, bottommargin = 0, dx = 1, dy = 4 },
             new Slope { leftmargin = 1, bottommargin = 0, dx = 1, dy = 6 },
             new Slope { leftmargin = 2, bottommargin = 2, dx = 8, dy = 8 },
-            new Slope { leftmargin = 0, bottommargin = 0, dx = 0, dy = 1 }
+            vertSlope
         };
 
         public CircleAprroximator(double _r) { r = _r; errorVal = (double)(r) * r; }
 
+        //public void Generate(StringBuilder sb, int height, 
+
+        private Dictionary<(int x, int y, int fwidth,  int fy), (double err, Slope s)> m_cache = new Dictionary<(int x, int y, int fwidth, int fy), (double, Slope)>();
 
 
-        private Dictionary<(int x, int y, int bm), (double err, Slope s)> m_cache = new Dictionary<(int x, int y, int bm), (double, Slope)>();
-
-        private double Recurse(int x, int y, int bottomMargin)
+        private void Calc()
         {
-            if (y==0) return 0; //target achieved
-            if (y-bottomMargin < 0 || x < 0 || x > Math.Ceiling(r)) return errorVal;
+            int bestr = 0;
+            int bestSlope = 0;
+            double bestError = double.MaxValue;
+            var EvenSlopes = m_slopes.Where(s => s.dx == s.dy && (s.dx & 1) == 0);
+            var OddSlopes = m_slopes.Where(s => s.dx == s.dy && (s.dx & 1) == 0);
+            int lowlimit = (int)Math.Ceiling((r - 2) / Math.Sqrt(2.0));
+            int uplimit = (int)Math.Floor((r + 2) / Math.Sqrt(2.0));
+
+            for (int r = lowlimit; r <= uplimit; r++)
+            {
+                double candidateError = Recurse(r, r, 0, 1);
+                if (candidateError < bestError)
+                {
+                    bestError = candidateError;
+                    bestSlope = 0;
+                    bestr = r;
+                }
+                foreach (Slope s in EvenSlopes)
+                {
+                    int newx = r + s.dx / 2;
+                    int newy = r + s.dy / 2;
+                    candidateError = CalcError((newx, newy), (r, r)) + Recurse(newx,newy, 0, 1);
+                    if (candidateError < bestError)
+                    {
+                        bestError = candidateError;
+                        bestSlope = s.dx;
+                        bestr = r;
+                    }
+                }
+            }
+            for (double r = lowlimit - 0.5; r <= uplimit + 0.5; r += 1.0)
+            {
+                foreach (Slope s in OddSlopes)
+                {
+                    int newx = (int)(r + s.dx * 0.5);
+                    int newy = (int)(r + s.dy * 0.5);
+                    double candidateError = CalcError((newx, newy), (r, r)) + Recurse(newx, newy, 0, 1);
+                    if (candidateError < bestError)
+                    {
+                        bestError = candidateError;
+                        bestSlope = s.dx;
+                        bestr = (int)r;
+                    }
+                }
+            }
+        }
+
+        private double Recurse(int x, int y, int fx, int fy)
+        {
+            if (y - fy < 0 || x < 0 || x > Math.Ceiling(r)) return errorVal;
+            if (y == 0) return 0; //target achieved
             //stay within a band of 2 plates
             double rxy = Math.Sqrt((double)(x)*x+(double)(y)*y);
             if (Math.Abs(rxy - r) > 2) return errorVal;
-            var key = (x, y, bottomMargin);
+            var key = (x, y, fx, fy);
             if (m_cache.TryGetValue(key,out (double,Slope) val))
             {
                 return val.Item1;
@@ -64,14 +118,34 @@ namespace BrunnianLink
 
             Slope bestSlope = m_slopes[0];
             double bestError = double.MaxValue;
-            
-            foreach (Slope slope in m_slopes)
+
+            foreach(Slope slope in m_slopes) 
             {
-                if (bottomMargin > 0 && slope.leftmargin > 0) continue;
+                if (fy > 0 && slope.leftmargin > fx) continue;
                 int newX = x + slope.dx;
                 int newY = y - slope.dy;
-                int newBm = Math.Max(0, bottomMargin - slope.dy);
-                double candidateError = CalcError((newX,newY), (x,y)) + Recurse(newX,newY,newBm);
+                int newfy = slope.bottommargin;
+                int newfx = 0;
+                if(slope==horzSlope && fy>0) //horz
+                {
+                    newfy = fy;
+                    newfx = fx + 1;
+                    if (newfx >= 2) //no limit imposed...
+                    {
+                        newfy = newfx = 0;
+                    }
+                }
+                if (slope == vertSlope && fy>0) //vert
+                {
+                    newfy = fy - 1;
+                    newfx = fx;
+                    if (newfy <=0)//no limit imposed...
+                    {
+                        newfy = newfx = 0;
+                    }
+
+                }
+                double candidateError = CalcError((newX,newY), (x,y)) + Recurse(newX,newY,newfx,newfy);
                 if (candidateError < bestError)
                 {
                     bestError = candidateError;
@@ -92,15 +166,16 @@ namespace BrunnianLink
             if (Math.Abs(alpha2 - alpha1) < epsilon)
                 return 0.0;
             System.Diagnostics.Debug.Assert(alpha2 > alpha1);
+            double D = p1.x * p2.y - p2.x * p1.y;
+
             if (r1 < (r+epsilon) && r2 < (r+epsilon)) //segment entirely inside
             {
-                return SectorArea(r, alpha1, alpha2) - TriangleArea(p1, p2);
+                return SectorArea(r, alpha1, alpha2) -0.5*D;
             }
             double dx = p2.x- p1.x;
             double dy = p2.y- p1.y; 
             System.Diagnostics.Debug.Assert(dy >  epsilon);
             double dr2 = dx * dx + dy * dy;
-            double D = p1.x * p2.y - p2.x * p1.y;
             double rootArg = r*r*dr2-D*D;
             if (rootArg < -epsilon) //no intersection
             {
