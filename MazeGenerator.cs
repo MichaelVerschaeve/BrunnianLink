1b1024bb09236844f51f1ec493ca4b05ece02808
Michael Verschaeve <Michael.Verschaeve@iba-ag.com>
2024-07-24 18:09:49 +0200

    extending mazes...

--- MazeGenerator.cs
+++ MazeGenerator.cs
@@ -230,5 +230,124 @@ namespace BrunnianLink
                     }
                 }
         }
+
+        public void GenerateSloped(StringBuilder sb)
+        {
+            MetaData.StartSubModel(sb, $"SlopedMaze_{n}x{n}");
+            Shape HorzStraight = new() { PartID = "HorizontalStraight" };
+            Shape VertStraight = HorzStraight.Rotate();
+            Shape CornerDtoR= new() { PartID = "Corner" };
+            Shape CornerRtoU = CornerDtoR.Rotate();
+            Shape CornerUtoL = CornerDtoR.Rotate(180);
+            Shape CornerLtoD = CornerDtoR.Rotate(-90);
+            Shape TshapeDown = new() { PartID = "TshapeDown" };
+            Shape TshapeRight = TshapeDown.Rotate();
+            Shape TshapeUp = TshapeDown.Rotate(180);
+            Shape TshapeLeft = TshapeDown.Rotate(-90);
+            Shape Cross = new() { PartID = "Cross" };
+            Shape TEndDown = new() { PartID = "TEnd" };
+            Shape TEndRight = TEndDown.Rotate();
+            Shape TEndUp = TEndDown.Rotate(180);
+            Shape TEndLeft = TEndDown.Rotate(-90);
+
+
+            int redId = ColorMap.Get("Red").id;
+            int grayID = ColorMap.Get("Light_Bluish_Grey").id;
+
+            if (n == 15)
+            {
+                BasePlate bp = new(32);
+
+                for (int x = 0; x < 2; x++)
+                    for (int y = 0; y < 2; y++)
+                        sb.AppendLine(bp.Print(15 + 32 * x, 15 + 32 * y, -3, grayID));
+            }
+            else if (n == 11)
+            {
+                BasePlate bp = new(48);
+                sb.AppendLine(bp.Print(23, 23, -3, grayID));
+            }
+
+            //corners
+            sb.AppendLine(CornerDtoR.Print(0,0,0,redId));
+            sb.AppendLine(CornerRtoU.Print(0, 4*n, 0, redId));
+            sb.AppendLine(CornerUtoL.Print(4*n, 4 * n, 0, redId));
+            sb.AppendLine(CornerLtoD.Print(4 * n, 4 * n, 0, redId));
+            //sides
+            for (int k = 1; k < n; k++)
+            {
+                if (maze[k, 0].Walls[(int)Direction.Left].Open)
+                    sb.AppendLine(HorzStraight.Print(4 * k, 0, 0, redId));
+                else
+                    sb.AppendLine(TshapeDown.Print(4*k,0,0,redId));
+
+                if (maze[k, n-1].Walls[(int)Direction.Left].Open)
+                    sb.AppendLine(HorzStraight.Print(4 * k, n, 0, redId));
+                else
+                    sb.AppendLine(TshapeUp.Print(4 * k, n, 0, redId));
+
+                if (maze[0, k].Walls[(int)Direction.Up].Open)
+                    sb.AppendLine(VertStraight.Print(0,4 * k, 0, redId));
+                else
+                    sb.AppendLine(TshapeRight.Print(0,4 * k, 0, redId));
+
+                if (maze[n-1, k].Walls[(int)Direction.Up].Open)
+                    sb.AppendLine(VertStraight.Print(n, 4 * k, 0, redId));
+                else
+                    sb.AppendLine(TshapeLeft.Print(n, 4 * k, 0, redId));
+            }
+            //mids
+            for (int x = 1; x < n; x++)
+            {
+                for (int y = 1; y < n; y++)
+                {
+                    Cell dr = maze[x, y];
+                    Cell ul = maze[x - 1, y - 1];
+                    HashSet<Direction> links = new HashSet<Direction>();
+                    if (ul.Walls[(int)Direction.Right].Open)
+                        links.Add(Direction.Up);
+                    if (ul.Walls[(int)Direction.Down].Open)
+                        links.Add(Direction.Left);
+                    if (dr.Walls[(int)Direction.Left].Open)
+                        links.Add(Direction.Down);
+                    if (dr.Walls[(int)Direction.Up].Open)
+                        links.Add(Direction.Right);
+
+                    Shape s = Cross;
+                    if (links.Count == 1) // t-shape
+                    {
+                        s = links.First() switch
+                        {
+                            Direction.Left => TshapeRight,
+                            Direction.Right => TshapeLeft,
+                            Direction.Down => TshapeUp,
+                            _ => TshapeDown
+                        };
+                    }
+                    else if (links.Count == 3) //end piece
+                    {
+                        if (!links.Contains(Direction.Left)) s = TEndLeft;
+                        else if (!links.Contains(Direction.Right)) s = TEndRight;
+                        else if (!links.Contains(Direction.Down)) s = TEndDown;
+                        else s = TEndUp;
+                    }
+                    else if (links.Count == 3) //straight or corner
+                    {
+                        if (!links.Contains(Direction.Left) && !links.Contains(Direction.Right)) s = HorzStraight;
+                        else if (!links.Contains(Direction.Left) && !links.Contains(Direction.Up)) s = CornerUtoL;
+                        else if (!links.Contains(Direction.Left) && !links.Contains(Direction.Down)) s = CornerLtoD;
+                        if (!links.Contains(Direction.Up) && !links.Contains(Direction.Down)) s = VertStraight;
+                        else if (!links.Contains(Direction.Right) && !links.Contains(Direction.Up)) s = CornerRtoU;
+                        else if (!links.Contains(Direction.Right) && !links.Contains(Direction.Down)) s = CornerDtoR;
+                    }
+                    sb.AppendLine(s.Print(4 * x, 4 * y, 0, redId));
+                }
+            }
+            //define subshapes...
+            MetaData.StartSubModel(sb, "HorizontalStraight");
+
+
+        }
+
     }
 }
