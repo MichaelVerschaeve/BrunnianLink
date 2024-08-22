2a720dcc73ab1fce33502a6e41c495877069e67c
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2024-08-23 00:55:05 +0200

    terdragonII implemented needs testing

--- TerDragonII.cs
+++ TerDragonII.cs
@@ -32,20 +32,31 @@ namespace BrunnianLink
 
             Rot30Coords currentPos = new();
 
-            while (rotations.Count>1)
+            while (rotations.Count > 1)
             {
-                if (rotations.Count > 2 && rotations[0] + rotations[1] == -rotations[2])
+                bool closed = rotations.Count > 2 && rotations[0] + rotations[1] == -rotations[2]; //closed triangle
+                bool counterclockwise = rotations[0].Rotate(120) == rotations[1];
+                int rotation = Enumerable.Range(0, 6).Select(t => 60 * t).First(a=>X.Rotate(a)==rotations[0]);
+                for (int i = 0; i < level; i++)
                 {
+                    if (counterclockwise)
+                        DrawTriangle(sb, currentPos.Rotate(i * 60), (rotation + i * 60) % 360, closed ? 3 : 1, colorIds[i], colorIds[(i + 1) % 6]);
+                    else
+                        DrawTriangle(sb, currentPos.Rotate(i * 60), (rotation + i * 60 + 300) % 360, closed ? 3 : 2, colorIds[(i + 1) % 6], colorIds[i]);
+                }
 
+                if (closed)
+                {
                     rotations.RemoveRange(0, 2);
                 }
                 else
                 {
-
                     currentPos += rotations[0];
                     rotations.RemoveAt(0);
                 }
             }
+
+            
 
             /*
             Shape wedgeLeft = new() { PartID = "65426" };  //   \|
@@ -54,11 +65,78 @@ namespace BrunnianLink
             */
         }
 
+        private static void DrawTriangle(StringBuilder sb, Rot30Coords c, int rotation, int cornerflags, int colorOutId, int colorInId)
+        {
+            switch (rotation)
+            {
+                case 0:
+                    DrawUp(sb, c.Cx + 3, c.Cy + 6, (cornerflags & 2) == 2, false, (cornerflags & 1) == 1, colorOutId, colorInId);
+                    break;
+                case 60:
+                    DrawDown(sb, c.Cx, c.Cy, (cornerflags & 2) == 2, (cornerflags & 1) == 1, false, colorOutId, colorInId);
+                    break;
+                case 120:
+                    DrawUp(sb, c.Cx - 3, c.Cy + 6, (cornerflags & 1) == 1, (cornerflags & 2) == 2, false, colorOutId, colorInId);
+                    break;
+                case 180:
+                    DrawDown(sb, c.Cx - 3, c.Cy - 6, (cornerflags & 1) == 1, false, (cornerflags & 2) == 2, colorOutId, colorInId);
+                    break;
+                case 240:
+                    DrawUp(sb, c.Cx, c.Cy, false, (cornerflags & 1) == 1, (cornerflags & 2) == 2, colorOutId, colorInId);
+                    break;
+                case 300:
+                    DrawDown(sb, c.Cx + 3, c.Cy - 6, false, (cornerflags & 1) == 1, (cornerflags & 2) == 2,  colorOutId, colorInId);
+                    break;
+
+            }
+        }
+
+        private static void DrawUp(StringBuilder sb, int x, int y, bool top, bool left, bool right, int colorOutId, int colorInId)
+        {
+            Shape wedgeLeft = new() { PartID = "65426" };  //   \|
+            Shape wedgeRight = new() { PartID = "65429" }; //   |/
+            Shape swLeft = new() { PartID = "24307" };  //   \|
+            Shape swRight = new() { PartID = "24299" }; //   |/
+            Plate p = new(2, 2);
+            sb.AppendLine(wedgeLeft.Rotate(180).Print(x+1,y-2,0,top?colorOutId:colorInId) );
+            sb.AppendLine(wedgeRight.Rotate(180).Print(x-1,y-2,0,top?colorOutId:colorInId) );
+            sb.AppendLine(p.Print(x , y - 5, 0, colorInId));
+            sb.AppendLine(swLeft.Rotate(180).Print(x + 2, y - 5, 0, right ? colorOutId : colorInId));
+            sb.AppendLine(swRight.Rotate(180).Print(x - 2, y - 5, 0, left ? colorOutId : colorInId));
+            DrawHex(sb, x, y - 4, colorInId);
+        }
+
+        private static void DrawDown(StringBuilder sb, int x, int y, bool left, bool right, bool bottom, int colorOutId, int colorInId)
+        {
+            Shape wedgeLeft = new() { PartID = "65426" };  //   \|
+            Shape wedgeRight = new() { PartID = "65429" }; //   |/
+            Shape swLeft = new() { PartID = "24307" };  //   \|
+            Shape swRight = new() { PartID = "24299" }; //   |/
+            Plate p = new(2, 2);
+            sb.AppendLine(wedgeLeft.Print(x - 1, y + 2, 0, bottom ? colorOutId : colorInId));
+            sb.AppendLine(wedgeRight.Print(x + 1, y + 2, 0, bottom ? colorOutId : colorInId));
+            sb.AppendLine(p.Print(x, y + 5, 0, colorInId));
+            sb.AppendLine(swLeft.Print(x - 2, y + 5, 0, left ? colorOutId : colorInId));
+            sb.AppendLine(swRight.Print(x + 2, y + 5, 0, right ? colorOutId : colorInId));
+            DrawHex(sb, x, y - 4, colorInId);
+        }
+
+        private static void DrawHex(StringBuilder sb, int x, int y, int colorInId)
+        {
+            Shape swLeft = new() { PartID = "24307" };  //   \|
+            Shape swRight = new() { PartID = "24299" }; //   |/
+            sb.AppendLine(swLeft.Rotate(180).Print(x + 1, y +1, 1,  colorInId));
+            sb.AppendLine(swRight.Rotate(180).Print(x - 1, y +1, 1, colorInId));
+            sb.AppendLine(swLeft.Print(x - 1, y - 1, 1, colorInId));
+            sb.AppendLine(swRight.Print(x + 1, y - 1, 1, colorInId));
+        }
+
+
 
         private static Rot30Coords X = new() { x = 1 };
         private static Rot30Coords U = new() { u = 1 };
-        private static Rot30Coords V = new() { v = 1 };
-        private static Rot30Coords Y = new() { y = 1 };
+        //private static Rot30Coords V = new() { v = 1 };
+        //private static Rot30Coords Y = new() { y = 1 };
 
 
         public struct Rot30Coords
@@ -67,8 +145,8 @@ namespace BrunnianLink
             public int u;
             public int v;
             public int y;
-            public readonly int Cx => 4 * x + 4 * u + 2 * v;
-            public readonly int Cy => 2 * u + 4 * v + 4 * y;
+            public readonly int Cx => 6 * (x + u) + 3 * v;
+            public readonly int Cy => 6 * (v + y) + 3 * u;
 
             public readonly Rot30Coords Rotate(int rot)
             {
@@ -89,14 +167,24 @@ namespace BrunnianLink
             public static Rot30Coords operator *(Rot30Coords a, int s)
             => new() { x = s * a.x, u = s * a.u, v = s * a.v, y = s * a.y };
 
-            public static bool operator==(Rot30Coords a, Rot30Coords b)
+            public static bool operator ==(Rot30Coords a, Rot30Coords b)
             => (a.x, a.u, a.v, a.y) == (b.x, b.u, b.v, b.y);
-            public static bool operator!=(Rot30Coords a, Rot30Coords b)
-            => (a.x, a.u, a.v, a.y)!=(b.x, b.u, b.v, b.y);
+            public static bool operator !=(Rot30Coords a, Rot30Coords b)
+            => (a.x, a.u, a.v, a.y) != (b.x, b.u, b.v, b.y);
 
             public Rot30Coords(int x, int u, int v, int y)
             {
                 this.x = x; this.u = u; this.v = v; this.y = y;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override int GetHashCode()
+            {
+                throw new NotImplementedException();
             }
         }
 
