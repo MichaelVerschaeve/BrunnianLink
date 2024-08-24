8b6e0b48d6c27ab1d03a71d2cfac47bbe3238e09
Michael Verschaeve <Michael.Verschaeve@telenet.be>
2024-08-24 13:40:47 +0200

    warnings

--- HeighwayTruchetNested.cs
+++ HeighwayTruchetNested.cs
@@ -14,14 +14,14 @@ namespace BrunnianLink
             // List<string> someColorNames = new() { "Bright_Light_Orange", "Light_Bluish_Grey", "Coral", "Dark_Turquoise" };
             List<string> someColorNames = new() { "Coral", "Lime", "Dark_Turquoise", "Bright_Light_Orange" };
             var colorIds = someColorNames.Select(x => ColorMap.Get(x).id).ToList();
-            Tile t = new Tile(1, 1);
+            Tile t = new(1, 1);
             sb.AppendLine(t.Print(0.5, -0.5, 0, colorIds[0]));
             sb.AppendLine(t.Print(0.5, 0.5, 0, colorIds[1]));
             sb.AppendLine(t.Print(-0.5, 0.5, 0, colorIds[2]));
             sb.AppendLine(t.Print(-0.5, -0.5, 0, colorIds[3]));
 
 
-            List<char> turns = new List<char>() { '+' };
+            List<char> turns = new() { '+' };
             for (int i = 0; i < level; i++)
                 turns = turns.Append('+').Concat(turns.Select(c => c == '+' ? '-' : '+').Reverse()).ToList();
 
@@ -64,20 +64,20 @@ namespace BrunnianLink
 
             foreach (var pair in board)
             {
-                (int x, int y) pos = pair.Key;
+                (int x, int y) = pair.Key;
                 Shape cutout = new() { PartID = "3396" };
                 if (pair.Value[1] != -1 || pair.Value[3] != -1)
                     cutout = cutout.Rotate();
-                sb.AppendLine(cutout.Print(2 * pos.x, 2 * pos.y, 0, pair.Value[4]));
+                sb.AppendLine(cutout.Print(2 * x, 2 * y, 0, pair.Value[4]));
                 Shape q = new() { PartID = "25269" };
                 if (pair.Value[0] != -1)
-                    sb.AppendLine(q.Rotate(270).Print(2 * pos.x + 0.5, 2 * pos.y + 0.5, 0, pair.Value[0]));
+                    sb.AppendLine(q.Rotate(270).Print(2 * x + 0.5, 2 * y + 0.5, 0, pair.Value[0]));
                 if (pair.Value[1] != -1)
-                    sb.AppendLine(q.Print(2 * pos.x - 0.5, 2 * pos.y + 0.5, 0, pair.Value[1]));
+                    sb.AppendLine(q.Print(2 * x - 0.5, 2 * y + 0.5, 0, pair.Value[1]));
                 if (pair.Value[2] != -1)
-                    sb.AppendLine(q.Rotate(90).Print(2 * pos.x - 0.5, 2 * pos.y - 0.5, 0, pair.Value[2]));
+                    sb.AppendLine(q.Rotate(90).Print(2 * x - 0.5, 2 * y - 0.5, 0, pair.Value[2]));
                 if (pair.Value[3] != -1)
-                    sb.AppendLine(q.Rotate(180).Print(2 * pos.x + 0.5, 2 * pos.y - 0.5, 0, pair.Value[3]));
+                    sb.AppendLine(q.Rotate(180).Print(2 * x + 0.5, 2 * y - 0.5, 0, pair.Value[3]));
             }
         }
     }
