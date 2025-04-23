84b3e6250549283d0bad5e21e3e5d509a031d7df
Michael Verschaeve <Michael.Verschaeve@iba-ag.com>
2025-04-23 20:26:05 +0200

    meh

--- Hilbert3D.cs
+++ Hilbert3D.cs
@@ -8,8 +8,71 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BrunnianLink
 {
+
     static public class Hilbert3D
     {
+        static bool ShapeCondition(List<int[]> source)
+        {
+            int dxm2 = source[~1][0] - source[~2][0];
+            int dym2 = source[~1][1] - source[~2][1];
+            int dzm2 = source[~1][2] - source[~2][2];
+            int dxm1 = source[0][0] - source[~1][0];
+            int dym1 = source[0][1] - source[~1][1];
+            int dzm1 = source[0][2] - source[~1][2];
+            int dx0 = source[1][0]- source[0][0];
+            int dy0 = source[1][1] - source[0][1];
+            int dz0 = source[1][2] - source[0][2];
+            int dx1 = source[2][0] - source[1][0];
+            int dy1 = source[2][1] - source[1][1];
+            int dz1 = source[2][2] - source[1][2];
+
+            //last segment perpendicular to first
+            if (dxm1 * dx0 + dym1 * dy0 + dzm1 * dz0 != 0)
+                return false;
+            //2nd segment perpendicular to first
+            if (dx1 * dx0 + dy1 * dy0 + dz1 * dz0 != 0)
+                return false;
+            //2nd segment perpendicual to last
+            if (dx1 * dxm1 + dy1 * dym1 + dz1 * dzm1 != 0)
+                return false;
+            //second to last segment = - first segment
+            if (dxm2 != dx0 || dym2 != dy0 || dzm2 != dz0)
+                return false;
+            return true;
+        }
+
+        static int[] MatMul(int[][] columnOrderFirst, int[] v)
+        {
+            int[] res = [0, 0, 0];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    res[i] += columnOrderFirst[j][i] * v[j];
+            return res;
+        }
+
+        static List<int[]> Transform(List<int[]> source, int[] targetOrigin, int[] targetX, int[] targetY, int[] targetZ)
+        {
+            if (!ShapeCondition(source))
+            {
+                source = new(source);
+                source.Reverse();
+            }
+            if (!ShapeCondition(source))
+                throw new NotImplementedException(); //not expected
+            int[][] transpose = 
+        }
+
+
+        static List<int[]> PointCollection(int level)
+        {
+            if (level == 1) return [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 1, 1], [1, 1, 1], [1, 0, 1], [0, 0, 1]];
+
+            List<int[]> points = PointCollection(level-1);
+
+        }
+
+
+
         static public void Generate(StringBuilder sb, int level)
         {
             level = Math.Max(level, 1);
